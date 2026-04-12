8afc135a23302c0a000e7420a1d0470784a49e81
wandi1209 <wandider@gmail.com>
2026-04-12 17:03:49 +0700

    Jobsheet 7 Praktikum 3 : Implementasi Tabs pada Info List di Filament

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -6,6 +6,8 @@ use Filament\Infolists\Components\IconEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 use Filament\Schemas\Schema;
 
 class ProductInfolist
@@ -14,6 +16,61 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tab')
+                ->tabs([
+                    Tab::make('Product Details')
+                        ->schema([
+                            TextEntry::make('name')
+                                ->label('Product Name')
+                                ->weight('bold')
+                            ->color('primary'),
+                            TextEntry::make('id')
+                                ->label('Product ID'),
+                            TextEntry::make('sku')
+                                ->label('Product SKU')
+                                ->badge()
+                                ->color('warning'),
+                            TextEntry::make('description')
+                                ->label('Description'),
+                            TextEntry::make('created_at')
+                                ->label('Product Creation Date')
+                                ->date('d M Y'),
+                    ]),
+                    Tab::make('Pricing & Stock')
+                        ->schema([
+                            TextEntry::make('price')
+                                ->label('Product Price')
+                                ->weight('bold')
+                                ->color('primary')
+                                ->icon('heroicon-o-currency-dollar')
+                                ->formatStateUsing(fn ($state) => 'Rp.' . number_format($state, 0, ',', '.')),
+                            TextEntry::make('stock')
+                                ->label('Product Stock')
+                                ->icon('heroicon-o-archive-box'),
+                    ]),
+                    Tab::make('Media & Status')
+                        ->schema([
+                            ImageEntry::make('image')
+                                    ->label('Product Image')
+                                    ->disk('public'),
+                                TextEntry::make('price')
+                                    ->label('Product Price')
+                                    ->weight('bold')
+                                    ->color('primary')
+                                    ->icon('heroicon-o-currency-dollar')
+                                    ->formatStateUsing(fn ($state) => 'Rp.' . number_format($state, 0, ',', '.')),
+                                TextEntry::make('stock')
+                                    ->label('Product Stock')
+                                    ->weight('bold')
+                                    ->color('primary'),
+                                IconEntry::make('is_active')
+                                    ->label('Active')
+                                    ->boolean(),
+                                IconEntry::make('is_featured')
+                                    ->label('Featured')
+                                    ->boolean(),
+                        ])
+                ])->columnSpanFull()->vertical(),
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
@@ -41,7 +98,7 @@ class ProductInfolist
                             ->formatStateUsing(fn ($state) => 'Rp.' . number_format($state, 0, ',', '.')),
                         TextEntry::make('stock')
                             ->label('Product Stock')
-                            ->icon('heroicon-o-collection'),
+                            ->icon('heroicon-o-archive-box'),
                 ])->columnSpanFull(),
                 Section::make('Media & Status')
                     ->description('')
